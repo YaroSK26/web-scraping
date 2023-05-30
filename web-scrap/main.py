2e7010e0ce1d4cbd51ebe0a2edc3547c93bfd54b
Yaro <jaroba0@gmail.com>
2023-05-30 19:11:46 +0200

    web scraping diel 19-24

--- web-scrap/main.py
+++ web-scrap/main.py
@@ -1,8 +1,29 @@
 from bs4 import BeautifulSoup
 
-with open("web-scrap/index.html") as file:
+with open("./index.html") as file:
     html_code = file.read()
 
 soup = BeautifulSoup(html_code, "html.parser")
 
 print(soup.title.string)
+
+
+
+all_a = soup.find_all("a")
+print(all_a)
+
+with open("./a.txt", mode="w" ) as fileA:
+    for one_a in all_a:
+        a1 = one_a.get("href")
+
+        fileA.write(a1)
+        fileA.write("\n")
+
+heading = soup.find(name="h1", id="name") 
+print(heading)
+
+heading2= soup.find_all("h2", class_="about")
+print(heading2)
+
+contact = soup.select("p a")
+# a ktore je v p tagu .. je aj select_one
